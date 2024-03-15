a9091bc71ce4a860dd49cfef8391d48b92447397
Haru <aezmedina3@gmail.com>
2024-03-15 10:12:49 +0800

    update 2

--- app/src/main/java/com/example/codefest/Login.java
+++ app/src/main/java/com/example/codefest/Login.java
@@ -3,12 +3,24 @@ package com.example.codefest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
+    private EditText EmailInput;
+    private EditText PasswordInput;
+
+    private String tempEmail;
+    private String tempPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
     }
+
+
+
 }

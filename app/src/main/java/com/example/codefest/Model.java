01431b030112c093d72dcf7a0c9d5c2822861dec
Haru <aezmedina3@gmail.com>
2024-03-15 11:44:51 +0800

    updateeeeeeee

---
+++ app/src/main/java/com/example/codefest/Model.java
@@ -0,0 +1,22 @@
+package com.example.codefest;
+
+public class Model {
+    public String email;
+    public String password;
+
+    public void setEmail(String mail){
+        email = mail;
+    }
+
+    public void setPassword(String pass){
+        password = pass;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}

f9afbe743586d4658cf6048a54aae2dc6ce7b2b8
ArchivalTertius <archivaltertius@gmail.com>
2024-03-15 13:19:04 +0800

    jiorertert

---
+++ app/src/main/java/com/example/codefest/lib.java
@@ -0,0 +1,14 @@
+package com.example.codefest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lib extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib);
+    }
+}
